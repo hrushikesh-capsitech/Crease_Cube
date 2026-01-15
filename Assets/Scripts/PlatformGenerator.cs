f85cd862f3f36c57f2928d48ba65d21773f5033b
VisheshKumawat <vishesh.kumawat@capsitech.com>
2026-01-15 17:01:30 +0530

    Platform Generator file updated

--- Assets/Scripts/PlatformGenerator.cs
+++ Assets/Scripts/PlatformGenerator.cs
@@ -1,6 +1,8 @@
 using Firebase.Analytics;
+using System.Collections;
 using UnityEngine;
-
+using UnityEngine.EventSystems;
+ 
 public class PlatformGenerator : MonoBehaviour
 {
 
@@ -12,7 +14,8 @@ public class PlatformGenerator : MonoBehaviour
 
     [SerializeField] private float growSpeed = 2f;
     [SerializeField] private float maxHeight = 4f;
-
+    [SerializeField] private float MoveSpeed = 1.5f;
+    [SerializeField] private GameObject BridgeParent;
 
 
     private Vector3 startScale;
@@ -21,20 +24,68 @@ public class PlatformGenerator : MonoBehaviour
     private Vector3 firstScale;
     private Vector3 firstPos;
     public bool isActive = true;
+
+    private bool SwitchToOpposite = false;
+    public bool isMoving = false;
+    public bool isMovingUp = false;
+    private bool isMovingLeft = false;
+    private bool isMovingRight = false;
+    float distance = 3f;
+    float travelDis = 0f;
+    public bool isFallingCube = false;
+    private Vector3 spawnPos;
     void Start()
     {
         startScale = PlatformPrefab.transform.localScale;
         firstPos = PlatformPrefab.transform.localPosition;
-       // PlatFormPrefabAtStart = Instantiate(PlatformPrefab);
+        // PlatFormPrefabAtStart = Instantiate(PlatformPrefab);
+        // SpawnPoint.transform.localPosition = PlatformPrefab.transform.localPosition;
+        spawnPos = PlatformPrefab.transform.position;
     }
 
     void Update()
     {
+
+        if (EventSystem.current.IsPointerOverGameObject()) return;
+
+        if (!isMoving)
+        {
+            StopCoroutine(leftMoveRoutine());
+            StopCoroutine(rightMoveRoutine());
+        }
+
+        if (isMoving && (isMovingLeft || isMovingRight))
+        {
+            if ((distance - travelDis) >= 0.03f)
+            {
+                Debug.Log("movement running function is acxtive");
+                travelDis += Time.deltaTime * MoveSpeed;
+                if (isMovingRight) transform.position += new Vector3(Time.deltaTime * MoveSpeed, 0f, 0f);
+                else
+                {
+                    transform.position -= new Vector3(Time.deltaTime * MoveSpeed, 0f, 0f);
+                }
+            }
+            else
+            {
+                if (isMovingRight)
+                {
+                    isMovingRight = false;
+                    StartCoroutine(leftMoveRoutine());
+                }
+                else
+                {
+                    isMovingLeft = false;
+                    StartCoroutine(rightMoveRoutine());
+                }
+            }
+
+        }
         if (!isActive || isReleased) return;
 
         if (Input.GetMouseButtonDown(0))
         {
-                isGrowing = true;
+            isGrowing = true;
         }
 
         if (isGrowing)
@@ -49,6 +100,8 @@ public class PlatformGenerator : MonoBehaviour
             isReleased = true;
             ReleasePlatform();
         }
+
+
     }
 
     public void DisablePlatform()
@@ -59,20 +112,21 @@ public class PlatformGenerator : MonoBehaviour
 
     public void GrowPlatFormSize()
     {
+        PlatformPrefab.SetActive(true);
         float delta = growSpeed * Time.deltaTime;
 
         float newHeight = PlatformPrefab.transform.localScale.y + delta;
-        newHeight = Mathf.Min(newHeight,maxHeight);
+        newHeight = Mathf.Min(newHeight, maxHeight);
 
         float heightChange = newHeight - PlatformPrefab.transform.localScale.y;
 
-       PlatformPrefab.transform.localScale = new Vector3(
-            PlatformPrefab.transform.localScale.x,
-            newHeight,
-            PlatformPrefab.transform.localScale.z
-        );
+        PlatformPrefab.transform.localScale = new Vector3(
+             PlatformPrefab.transform.localScale.x,
+             newHeight,
+             PlatformPrefab.transform.localScale.z
+         );
 
-        PlatformPrefab.transform.position += Vector3.up * (heightChange / 1.3f);
+        PlatformPrefab.transform.localPosition += Vector3.up * (heightChange / 2f);
     }
 
     public void ReleasePlatform()
@@ -83,12 +137,12 @@ public class PlatformGenerator : MonoBehaviour
         {
             hg.connectedBody = transform.gameObject.GetComponent<Rigidbody>();
         }
-        if(rb != null)
+        if (rb != null)
         {
             rb.isKinematic = false;
             rb.useGravity = true;
         }
-        rb.AddForce(new Vector3(5f,0f,0f),ForceMode.Impulse);
+        rb.AddForce(new Vector3(5f, 0f, 0f), ForceMode.Impulse);
 
         if (FireBaseManager.IsFirebaseReady)
         {
@@ -115,10 +169,63 @@ public class PlatformGenerator : MonoBehaviour
 
     public void ResetPlatform()
     {
-        if(PlatformPrefab != null)
+        if (PlatformPrefab != null)
         {
             Destroy(PlatformPrefab);
         }
-        PlatformPrefab = Instantiate(platFormPrefabFromAssets, platFormPrefabFromAssets.transform.position, platFormPrefabFromAssets.transform.rotation,transform);
+        PlatformPrefab = Instantiate(platFormPrefabFromAssets, spawnPos, platFormPrefabFromAssets.transform.rotation, BridgeParent.transform);
+        PlatformPrefab.tag = "platform";
+        PlatformPrefab.SetActive(false);
+        isReleased = false;
+    }
+
+    public void moveCube()
+    {
+        isMoving = true;
+        isMovingRight = true;
+        MoveSpeed = Random.Range(1f, 2f);
+    }
+
+    IEnumerator rightMoveRoutine()
+    {
+        distance = 3f;
+        travelDis = 0f;
+        MoveSpeed = Random.Range(1f, 2f);
+        yield return new WaitForSeconds(2f);
+        isMovingRight = true;
+    }
+
+    IEnumerator leftMoveRoutine()
+    {
+        distance = 3f;
+        travelDis = 0f;
+        MoveSpeed = Random.Range(1f, 2f);
+        yield return new WaitForSeconds(2f);
+        isMovingLeft = true;
+    }
+
+    public void ActivateMovingCube()
+    {
+        Debug.Log("Falling cube is active");
+        isFallingCube = true;
+        StartCoroutine(GameOverDelayifNotMoves());
+    }
+
+    public void StopFalling()
+    {
+        StopAllCoroutines();
+    }
+
+    IEnumerator GameOverDelayifNotMoves()
+    {
+        yield return new WaitForSeconds(5f);
+        Debug.Log("Falling cube is active and the active cube is also a child");
+        isActive = false;
+        PlatformPrefab.tag = "Untagged";
+        PlatformPrefab.SetActive(false);
+        gameObject.GetComponent<Rigidbody>().isKinematic = false;
+        gameObject.GetComponent<Rigidbody>().useGravity = true;
+        yield return new WaitForSeconds(2f);
+        GameManager.Instance.GameOver();
     }
 }
